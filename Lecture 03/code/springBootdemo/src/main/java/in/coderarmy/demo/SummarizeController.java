a7273928e818ec3293e8d440f4133d34fe475040
Aditya Tandon <40351467+adityatandon15@users.noreply.github.com>
2026-09-10 15:58:25 +0530

    Add files via upload

---
+++ Lecture 03/code/springBootdemo/src/main/java/in/coderarmy/demo/SummarizeController.java
@@ -0,0 +1,17 @@
+package in.coderarmy.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class SummarizeController {
+
+    @Autowired
+    private SummarizeService summarizeService;
+
+    @PostMapping("/summarize")
+    public String summarize(@RequestBody String ticket) {
+        return summarizeService.summarize(ticket);
+    }
+}
